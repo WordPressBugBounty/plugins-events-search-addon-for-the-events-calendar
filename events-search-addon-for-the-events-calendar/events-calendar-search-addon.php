5cc2ac2867a2f1fcff88c9426e8cc2a53c7fec3a
Sean Murphy <sean@iamseanmurphy.com>
2025-06-13 10:18:04 +0000

    1.2.12

--- events-search-addon-for-the-events-calendar/events-calendar-search-addon.php
+++ events-search-addon-for-the-events-calendar/events-calendar-search-addon.php
@@ -3,17 +3,18 @@
  * Plugin Name: The Events Calendar Search Addon
  * Description: A simple events search box to find any event quickly for The Events Calendar Free Plugin (by MODERN TRIBE) - <strong>[events-calendar-search placeholder="Search Events" show-events="5" disable-past-events="false" layout="medium" content-type="advance" ]</strong>
  * Plugin URI: https://eventscalendaraddons.com/
- * Version: 1.2.11
+ * Version: 1.2.12
  * Requires at least: 5.0
  * Tested up to: 6.7.1
  * Requires PHP: 5.6
- * Stable tag: 1.2.11
+ * Stable tag: 1.2.12
  * Author: Cool Plugins
  * Author URI: https://coolplugins.net
  * License: GPL2
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: ecsa
  * Domain Path: languages
+ * Requires Plugins: the-events-calendar
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -23,13 +24,14 @@ if ( defined( 'ECSA_VERSION' ) ) {
 	return;
 }
 
-define( 'ECSA_VERSION', '1.2.11' );
+define( 'ECSA_VERSION', '1.2.12' );
 define( 'ECSA_FILE', __FILE__ );
 define( 'ECSA_PATH', plugin_dir_path( ECSA_FILE ) );
 define( 'ECSA_URL', plugin_dir_url( ECSA_FILE ) );
+define( 'ECSA_FEEDBACK_API', 'https://feedback.coolplugins.net/' );
 
-register_activation_hook( ECSA_FILE, array( 'EventsCalendarSearchAddon', 'activate' ) );
-register_deactivation_hook( ECSA_FILE, array( 'EventsCalendarSearchAddon', 'deactivate' ) );
+register_activation_hook( ECSA_FILE, array( 'EventsCalendarSearchAddon', 'ecsa_activate' ) );
+register_deactivation_hook( ECSA_FILE, array( 'EventsCalendarSearchAddon', 'ecsa_deactivate' ) );
 
 
 /*
@@ -56,6 +58,7 @@ if ( ! class_exists( 'EventsCalendarSearchAddon' ) ) :
 		|--------------------------------------------------------------------------
 		*/
 		private function __construct() {
+			$this->ecsa_include_files();
 			add_action( 'init', array( $this, 'ecsa_load_textdomain' ) );
 			add_action( 'plugins_loaded', array( $this, 'ecsa_check_event_calender_installed' ) );
 			add_action( 'plugins_loaded', array( $this, 'includes' ) );
@@ -65,6 +68,10 @@ if ( ! class_exists( 'EventsCalendarSearchAddon' ) ) :
 			add_action( 'wp_ajax_nopriv_ecsa_search_data', 'ecsa_get_searchdata' );
 		}
 
+
+		public function ecsa_include_files(){
+			require_once ECSA_PATH . 'admin/cpfm-feedback/cron/class-cron.php';
+		}
 
 		/*
 		|--------------------------------------------------------------------------
@@ -76,6 +83,43 @@ if ( ! class_exists( 'EventsCalendarSearchAddon' ) ) :
 			if ( ! class_exists( 'Tribe__Events__Main' ) or ! defined( 'Tribe__Events__Main::VERSION' ) ) {
 				 add_action( 'admin_notices', array( $this, 'ecsa_Install_ECT_Notice' ) );
 			}
+			if (is_admin()) {
+				require_once ECSA_PATH . '/admin/feedback/admin-feedback-form.php';
+			}
+
+			if(!class_exists('CPFM_Feedback_Notice')){
+				require_once ECSA_PATH . 'admin/cpfm-feedback/cpfm-feedback-notice.php';
+			}
+	
+			add_action('cpfm_register_notice', function () {
+			
+				if (!class_exists('CPFM_Feedback_Notice') || !current_user_can('manage_options')) {
+					return;
+				}
+				$notice = [
+					'title' => __('Cool Plugins Events Addons', 'ecsa'),
+					'message' => __('Help us make this plugin more compatible with your site by sharing non-sensitive site data.', 'ecsa'),
+					'pages' => ['cool-plugins-events-addon'],
+					'always_show_on' => ['cool-plugins-events-addon'], // This enables auto-show
+					'plugin_name'=>'ecsa',
+					
+				];
+	
+				CPFM_Feedback_Notice::cpfm_register_notice('cool_events', $notice);
+	
+					if (!isset($GLOBALS['cool_plugins_feedback'])) {
+						$GLOBALS['cool_plugins_feedback'] = [];
+					}
+				
+					$GLOBALS['cool_plugins_feedback']['cool_events'][] = $notice;
+		   
+			});
+			add_action('cpfm_after_opt_in_ecsa', function($category) {
+	
+				if ($category === 'cool_events') {
+					ECSA_cronjob::ecsa_send_data();
+				}
+			});
 		}
 
 		public function ecsa_Install_ECT_Notice() {
@@ -162,6 +206,14 @@ if ( ! class_exists( 'EventsCalendarSearchAddon' ) ) :
 		*/
 		public function ecsa_load_textdomain() {
 			load_plugin_textdomain( 'ecsa', false, basename( dirname( __FILE__ ) ) . '/languages/' );
+			
+			if (!get_option( 'ecsa_initial_save_version' ) ) {
+                add_option( 'ecsa_initial_save_version', ECSA_VERSION );
+            }
+
+            if(!get_option( 'ecsa-install-date' ) ) {
+                add_option( 'ecsa-install-date', gmdate('Y-m-d h:i:s') );
+            }
 		}
 
 		/*
@@ -187,11 +239,29 @@ if ( ! class_exists( 'EventsCalendarSearchAddon' ) ) :
 		| Run when activate plugin.
 		|----------------------------------------------------------------------------
 		*/
-		public static function activate() {
+		public static function ecsa_activate() {
 			update_option( 'ecsa-v', ECSA_VERSION );
 			update_option( 'ecsa-type', 'FREE' );
 			update_option( 'ecsa-installDate', gmdate( 'Y-m-d h:i:s' ) );
 			update_option( 'ecsa-ratingDiv', 'no' );
+
+			$review_option = get_option("cpfm_opt_in_choice_cool_events");
+
+			if ($review_option === 'yes') {
+				if (!wp_next_scheduled('ecsa_extra_data_update')) {
+
+					wp_schedule_event(time(), 'every_30_days', 'ecsa_extra_data_update');
+
+				}
+			}
+
+			if (!get_option( 'ecsa_initial_save_version' ) ) {
+                add_option( 'ecsa_initial_save_version', ECSA_VERSION );
+            }
+
+            if(!get_option( 'ecsa-install-date' ) ) {
+                add_option( 'ecsa-install-date', gmdate('Y-m-d h:i:s') );
+            }
 		}
 
 
@@ -200,7 +270,10 @@ if ( ! class_exists( 'EventsCalendarSearchAddon' ) ) :
 		| Run when de-activate plugin.
 		|----------------------------------------------------------------------------
 		*/
-		public static function deactivate() {
+		public static function ecsa_deactivate() {
+			if (wp_next_scheduled('ecsa_extra_data_update')) {
+				wp_clear_scheduled_hook('ecsa_extra_data_update');
+			}
 		}
 
 	}
--- events-search-addon-for-the-events-calendar/readme.txt
+++ events-search-addon-for-the-events-calendar/readme.txt
@@ -5,7 +5,7 @@ Tags: the events calendar, events addons, event, search, calendar, modern tribe,
 Requires at least: 5.0
 Tested up to: 6.8.1
 Requires PHP: 5.6
-Stable tag: 1.2.11
+Stable tag: 1.2.12
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -86,6 +86,11 @@ If you want to customize the addon according to your own requirements, you can h
 1. The Events Calendar Search Addon Shortcode and Sidebar Widget View.
 
 == Changelog ==
+
+= Version 1.2.12 | June 13, 2025 =
+* Added: User opt-in option in plugin.
+* Added: Deactivate feedback notice.
+* Tested upto: WordPress version 6.8.1
 
 = Version 1.2.11 | 06 May 2025 =
 * Minor textual change in plugin header.
